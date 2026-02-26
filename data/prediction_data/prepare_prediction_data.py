b81009da0949807757763c940b42064aa6d114a0
vera-bernhard <veralara.bernhard@uzh.ch>
2026-02-26 11:01:09 +0100

    Update data id issue

--- data/prediction_data/prepare_prediction_data.py
+++ data/prediction_data/prepare_prediction_data.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 
+# Get the data that has been manually annotated as relevant (in ASReview) but was not manually annotated for classification/Ner
 
 def determine_not_annotated():
     data = 'data/prepared_data/psychedelic_study_relevant.csv'
@@ -12,6 +13,7 @@ def determine_not_annotated():
     with open(ids, 'r', encoding='utf-8') as f:
         ids = f.readlines()
     ids = [int(x.strip()) for x in ids]
+    print(f'Number of annotated studies: {len(ids)}')
 
     to_be_annotated = []
 
@@ -22,11 +24,31 @@ def determine_not_annotated():
             to_be_annotated.append(row)
 
     df = pd.DataFrame(to_be_annotated)
+    print(f'Number of studies to be annotated: {len(df)}')
+        
+    df.to_csv('data/prediction_data/studies_relevant_unannotated_20240101_00-00-00.csv', index=False)
 
-    df.to_csv('data/prediction_data/psychedelic_study_unannoated.csv', index=False)
+def get_all_relevant_studies_with_info():
+    data = 'data/prepared_data/psychedelic_study_relevant.csv'
+    studies_info = '/home/veral/PsyNamic/data/raw_data/dataset_relevant_cleaned.csv'
+
+    df = pd.read_csv(data)
+    studies_info_df = pd.read_csv(studies_info)
+
+    # merge with studies info to get the title and abstract, to_be_annotated contains 'id', studies_info_df contains 'record_id'
+    # add all columns from studies_info_df that are not in df, and merge on 'id' and 'record_id'
+    # do not keep the id column of the studies info
+    studies_info_df = studies_info_df.drop(columns=['id','text'])
+    df = pd.merge(df, studies_info_df, left_on='id', right_on='record_id', how='left')
+    # drop the record_id column
+    df = df.drop(columns=['record_id'])
+    print('Total number of relevant studies: ', len(df))
+
+    df.to_csv('data/prediction_data/studies_relevant_with_info_20240101_00-00-00.csv', index=False)
 
 def main():
     determine_not_annotated()
+    get_all_relevant_studies_with_info()
 
 if __name__ == '__main__':
     main()
